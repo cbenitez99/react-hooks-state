86c706cef3e4f29992f39424182fa7e29eac6ba0
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-07-23 19:49:14 -0500

    commit tree 7b71f3fa6dc62f9b93428fa27dff77319acb5f07

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; //importing {useState} function from React
+//It's a React hook because we are hooking into reacts internal state
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     setCount(count + 1);
+//   }
+
+//   return <button onClick={increment}>I have been clicked {count} times</button>;
+// }
 
 function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
-  }
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
+  } 
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
-}
+  return <div onClick={increment}>{count}</div>;
+} 
 
 export default Counter;
